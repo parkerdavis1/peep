c9834d448f5add51d71fb788df09d54fd521789e
Parker <parkied@gmail.com>
2026-05-03 16:54:25 -0700

    Add lightning css and browserslist

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
+
+export default {
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(">= 0.25%")),
+    },
+  },
+  build: {
+    cssMinify: "lightningcss",
+  },
+};
