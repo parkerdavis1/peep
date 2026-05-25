1f0f93b2c87827c2199fa18bcbac803836f5064f
Parker <parkied@gmail.com>
2026-05-25 12:29:02 -0700

    feat: add open graph info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onNavigate } from "$app/navigation";
   import "../app.css";
+  import metadata from "$lib/metadata.json";
 
   let { children } = $props();
 
@@ -15,5 +16,16 @@
     });
   });
 </script>
+
+<svelte:head>
+  <meta name="description" content={metadata.description} />
+  <!-- Open Graph -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={metadata.url} />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:title" content={metadata.title} />
+  <meta property="og:description" content={metadata.description} />
+  <meta property="og:image" content="/peep-social.png" />
+</svelte:head>
 
 {@render children()}
--- src/routes/about/+page.md
+++ src/routes/about/+page.md
@@ -2,7 +2,7 @@
 
 **peep** is a web app for editing recordings before uploading to eBird.
 
-It is designed to be a fast and easy way to perform [common editing tasks](https://ebird.freshdesk.com/en/support/solutions/articles/48001064341-audio-preparation-and-upload-guidelines):
+It is a fast and easy way to perform [common editing tasks](https://ebird.freshdesk.com/en/support/solutions/articles/48001064341-audio-preparation-and-upload-guidelines):
 - Trim the beginning and end of the recording
 - Apply a light high-pass filter to remove low-frequency noise
 - Normalize gain to -3dB
